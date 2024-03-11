ce72570e28cfa3064c061621e0a4a21363bf7fa5
TESSOH TENE <michel.tessohtene@cgi.com>
2024-03-11 15:29:35 +0100

    Initial commit

---
+++ src/test/java/org/mtt/springsecurityandangular/SpringSecurityAndAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mtt.springsecurityandangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAndAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
